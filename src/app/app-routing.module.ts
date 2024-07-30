e14537f7bf2024a8aeda933909ea6abe66967374
ashidthanikkal <ashidthanikkal@gmail.com>
2024-07-30 10:37:48 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SingleViewComponent } from './single-view/single-view.component';
+import { single } from 'rxjs';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"login",component:LoginComponent},
+  {path:"signup",component:SignUpComponent},
+  {path:"single-view/:id",component:SingleViewComponent},
+  {path:"cart",component:CartComponent},
+  {path:"checkout",component:CheckoutComponent},
+  {path:"wishlist",component:WishlistComponent},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CartComponent } from './cart/cart.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { SingleViewComponent } from './single-view/single-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
+    WishlistComponent,
+    CheckoutComponent,
+    SingleViewComponent
   ],
   imports: [
     BrowserModule,
